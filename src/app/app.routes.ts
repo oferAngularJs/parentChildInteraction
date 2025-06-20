02853444590cfbb853a6b622a97466f131f855fe
Ofer K <ofer_k2000@yahoo.com>
2025-06-20 19:04:37 +0300

    sparator the screen to three sections

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './views/main/main.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/main', pathMatch: 'full' },
+  { path: 'main', component: MainComponent },
+
+];
